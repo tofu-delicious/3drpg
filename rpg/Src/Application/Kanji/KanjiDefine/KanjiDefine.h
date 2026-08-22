00c5f3548dc697a3e0fa94cebfff6e18b862555a
tofudelicious <1386175@st.kobedenshi.ac.jp>
2026-08-22 21:30:46 +0900

    8月22日 部首・つくり・漢字管理システムの構築
    組み合わせゲームから方針が変わる可能性があるから、この管理システム使わないかも...

---
+++ rpg/Src/Application/Kanji/KanjiDefine/KanjiDefine.h
@@ -0,0 +1,31 @@
+﻿//各部首・つくり・部首とつくりによって作成できる漢字を列挙しているクラス
+#pragma once
+
+//部首
+enum class RadicalID
+{
+	None = -1,
+	Hi,				//火偏
+};
+
+//つくり
+enum class ElementID
+{
+	None = -1,
+	Chou,			//丁
+};
+
+//完成する漢字
+enum class KanjiID
+{
+	None = -1,
+	Tou,			//灯
+};
+
+//「部首 + つくり → 漢字」のレシピ
+struct KanjiRecipe
+{
+	RadicalID radical;
+	ElementID element;
+	KanjiID	  kanji;
+};
