ee120ce1e3244f36a920b31f5ecb6c85693da9b8
tofudelicious <1386175@st.kobedenshi.ac.jp>
2026-08-23 09:38:28 +0900

    8月23日 KanjiRecipe & KanjiInventoryのプログラム完全削除

--- rpg/Src/Application/Kanji/KanjiRecipe/KanjiRecipeTable.h
+++
@@ -1,6 +0,0 @@
-﻿//#pragma once
-//#include "../KanjiDefine/KanjiDefine.h"
-//
-//extern const std::vector<KanjiRecipe> m_KanjiRecipeTable;
-//
-//KanjiID FindKanji(RadicalID a_radical, ElementID a_element);
